5477dfea442a3b2f248be621c360a9f8fb875f57
Steve Johns <steve@stevejay.net>
2016-12-18 18:48:32 +0000

    refactor index.js

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 For building complete result sets from the AWS DynamoDB API DocumentClient class
 
 [![npm version](https://badge.fury.io/js/dynamodb-doc-client-wrapper.svg)](https://badge.fury.io/js/dynamodb-doc-client-wrapper)
+[![Codeship Status for stevejay/dynamodb-doc-client-wrapper](https://app.codeship.com/projects/d832c8d0-a77d-0134-c8f7-7eca77d71521/status?branch=master)](https://app.codeship.com/projects/191146)
 [![Coverage Status](https://coveralls.io/repos/github/stevejay/dynamodb-doc-client-wrapper/badge.svg?branch=master)](https://coveralls.io/github/stevejay/dynamodb-doc-client-wrapper?branch=master)
 [![dependency status](https://david-dm.org/stevejay/dynamodb-doc-client-wrapper.svg)](https://david-dm.org/stevejay/dynamodb-doc-client-wrapper)
 
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const BatchGetParamsTakeManager = require('./lib/BatchGetParamsTakeManager');
+
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
 function query(params) {
@@ -30,64 +32,13 @@ function _applyExclusiveStartAction(action, params) {
     return documentClient[action](paramsCopy).promise().then(resultHandler);
 }
 
-const BATCH_GET_MAX_TAKE = 100;
-
-class BatchGetParamsManager {
-    constructor(params) {
-        this.params = params;
-        this.nextIndexesLookup = {};
-        this.tableNames = Object.getOwnPropertyNames(params.RequestItems);
-
-        this.tableNames.forEach(tableName =>
-            this.nextIndexesLookup[tableName] = {
-                nextIndex: 0,
-                length: params.RequestItems[tableName].length
-            }
-        );
-    }
-
-    getTakeParams() {
-        let totalItems = 0;
-        const result = { RequestItems: {} };
-
-        for (let i = 0; i < this.tableNames.length; ++i) {
-            const tableName = this.tableNames[i];
-            const nextIndexes = this.nextIndexesLookup[tableName];
-
-            if (nextIndexes.nextIndex >= nextIndexes.length) {
-                continue;
-            }
-
-            const takeCount = Math.min(
-                nextIndexes.length - nextIndexes.nextIndex,
-                BATCH_GET_MAX_TAKE);
-
-            result.RequestItems[tableName] = Object.assign(
-                {}, this.params.RequestItems[tableName]);
-
-            result.RequestItems[tableName].Keys = this.params.RequestItems[tableName].Keys.slice(
-                nextIndexes.nextIndex,
-                nextIndexes.nextIndex + takeCount);
-
-            nextIndexes.nextIndex += takeCount;
-            totalItems += takeCount;
-
-            if (totalItems >= BATCH_GET_MAX_TAKE) {
-                break;
-            }
-        }
-
-        return totalItems === 0 ? null : result;
-    }
-}
-
 function batchGetImpl(params) {
     const result = { Responses: {} };
 
     const tableNames = Object.getOwnPropertyNames(params.RequestItems);
     tableNames.forEach(tableName => result.Responses[tableName] = []);
 
-    const batchTakeManager = new BatchGetParamsManager(params);
+    const batchTakeManager = new BatchGetParamsTakeManager(params);
     let takeParams = batchTakeManager.getTakeParams();
 
     let resultHandler = null;
